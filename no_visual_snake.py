97e9ef388ad074c5d8769a6124c32c0b1edd104d
Milan <58393068+Desoxyr@users.noreply.github.com>
2023-04-04 00:34:07 +0200

    Snape without visual

---
+++ no_visual_snake.py
@@ -0,0 +1,66 @@
+﻿import random
+
+from constants import *
+
+
+class Snake:
+    def __init__(self):
+
+        # Game State
+        self.alive = True
+        self.cause_of_death = None
+        self.score = 0
+
+        # Snake
+        self.snake_list = []
+        self.pos_x = DIS_WIDTH / 2
+        self.pos_y = DIS_HEIGHT / 2
+        self.move_x = 0
+        self.move_y = 0
+
+        # Create first food
+        self.food_x = round(random.randrange(0, DIS_WIDTH - BLOCK_SIZE) / 10.0) * 10.0
+        self.food_y = round(random.randrange(0, DIS_HEIGHT - BLOCK_SIZE) / 10.0) * 10.0
+
+    # TODO: Q Learning Action
+    def handle_action(self):
+        return
+
+    def move_snake(self):
+        self.pos_x += self.move_x
+        self.pos_y += self.move_y
+        snake_head = [self.pos_x, self.pos_y]
+        self.snake_list.append(snake_head)
+
+    def check_collision(self):
+        if self.pos_x >= DIS_WIDTH or self.pos_x < 0 or self.pos_y >= DIS_HEIGHT or self.pos_y < 0:
+            self.alive = False
+            self.cause_of_death = 'Out of bounds'
+
+        snake_head = [self.pos_x, self.pos_y]
+        if snake_head in self.snake_list[:-1]:
+            self.alive = False
+            self.cause_of_death = 'Hit Tail'
+
+    def handle_food(self):
+        if self.pos_x == self.food_x and self.pos_y == self.food_y:
+            self.food_x = round(random.randrange(0, DIS_WIDTH - MOVE_SPEED) / 10.0) * 10.0
+            self.food_y = round(random.randrange(0, DIS_HEIGHT - MOVE_SPEED) / 10.0) * 10.0
+            self.score += 1
+
+    def handle_tail(self):
+        snake_length = self.score + 1
+        if len(self.snake_list) > snake_length:
+            del self.snake_list[0]
+
+    def game_loop(self):
+        while self.alive:
+            self.handle_action()
+
+            self.move_snake()
+            self.check_collision()
+
+            self.handle_food()
+            self.handle_tail()
+
+        quit()
