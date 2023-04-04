c8e319c1a8ec3118132d9ad803e4be35588d5c6a
Milan <58393068+Desoxyr@users.noreply.github.com>
2023-04-05 01:28:04 +0200

    Added function for running saved model

--- QLearning/no_visual_snake.py
+++ QLearning/no_visual_snake.py
@@ -1,4 +1,6 @@
-﻿import random
+﻿import numpy as np
+import random
+import pickle
 
 from Common.constants import *
 from Common.direction import Direction
@@ -43,13 +45,6 @@ class NoVisualSnake:
 
         self.snake_list.append(self.snake_head())
 
-    def collision(self, x, y):
-        if x >= DIS_WIDTH or x < 0 or y >= DIS_HEIGHT or y < 0:
-            return True
-
-        if [x, y] in self.snake_list[:-1]:
-            return True
-
     def eat_food(self):
         if self.pos_x == self.food_x and self.pos_y == self.food_y:
             self.food_x = round(random.randrange(0, DIS_WIDTH - MOVE_SPEED) / 10.0) * 10.0
@@ -62,11 +57,12 @@ class NoVisualSnake:
         if len(self.snake_list) > self.snake_length():
             del self.snake_list[0]
 
-    def snake_head(self):
-        return [self.pos_x, self.pos_y]
+    def collision(self, x, y):
+        if x >= DIS_WIDTH or x < 0 or y >= DIS_HEIGHT or y < 0:
+            return True
 
-    def snake_length(self):
-        return self.score + 1
+        if [x, y] in self.snake_list[:-1]:
+            return True
 
     def get_state(self):
         snake_head = self.snake_head()
@@ -88,6 +84,12 @@ class NoVisualSnake:
         ]
 
         return tuple(state)
+
+    def snake_head(self):
+        return [self.pos_x, self.pos_y]
+
+    def snake_length(self):
+        return self.score + 1
 
     def is_safe(self, x, y):
         if self.collision(x, y):
@@ -112,4 +114,28 @@ class NoVisualSnake:
 
         return self.get_state(), reward, self.alive
 
-    # TODO new game run that loops and gets final length
+    def run_game(self, episode):
+        filename = f"pickle/qlearning/{episode}.pickle"
+        with open(filename, 'rb') as file:
+            q_table = pickle.load(file)
+
+        length = self.snake_length()
+        steps_without_food = 0
+
+        while self.alive:
+            state = self.get_state()
+
+            action = np.argmax(q_table[state])
+            action = Direction(action)
+            self.step(action)
+
+            if self.snake_length() != length:
+                steps_without_food = 0
+                length = self.snake_length()
+            else:
+                steps_without_food += 1
+
+            if steps_without_food == 1000:
+                break
+
+        return self.snake_length()
