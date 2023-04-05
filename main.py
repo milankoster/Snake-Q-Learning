2dc6e3aece4c36410d610aad9b19cbafe26377a0
Milan <58393068+Desoxyr@users.noreply.github.com>
2023-04-05 04:13:01 +0200

    Update main class with placeholders for 3 different options

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 ﻿# Press the green button in the gutter to run the script.
+from manual.manual_snake import ManualSnake
 from qlearning.q_learner import QLearner
+from qlearning.q_visualiser import AutoSnakeVisualiser
 
 if __name__ == '__main__':
-
-    q_learner = QLearner()
-    q_learner.train()
-
     # # Manual Snake
     # snake = ManualSnake()
     # snake.game_loop()
 
-    # # Snake Visualisation
-    # visual_snake = AutoSnakeVisualiser()
-    # visual_snake.run_game(10000)
+    # # Q learning (no visualisation)
+    # q_learner = QLearner()
+    # q_learner.train()
+
+    # Q Learning Visualisation
+    visual_snake = AutoSnakeVisualiser()
+    visual_snake.run_game(10000)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
