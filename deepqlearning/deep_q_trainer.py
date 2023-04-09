ab713b3ca75abac01d24d1272167a9013eff27fe
Milan <58393068+Desoxyr@users.noreply.github.com>
2023-04-09 03:24:24 +0200

    Apply different deep q algorithm

--- deepqlearning/deep_q_trainer.py
+++ deepqlearning/deep_q_trainer.py
@@ -12,79 +12,79 @@ from deepqlearning.deep_q_environment import DeepQEnvironment
 
 class DeepQTrainer:
     def __init__(self):
-        self.gamma = 0.85
+        self.gamma = 0.95
         self.epsilon = 1.0
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
-        self.learning_rate = 0.005
-        self.tau = .125
+        self.learning_rate = 0.00025
         self.batch_size = 512
 
-        self.trials = 1000
+        self.episodes = 1000
         self.trial_len = 500
+
+        # TODO Move these to env
+        self.state_space = 12
+        self.action_space = 4
 
         self.env = DeepQEnvironment()
         self.memory = deque(maxlen=2000)
         self.model = self.create_model()
         self.target_model = self.create_model()
 
-    # TODO Figure out input size
     def create_model(self):
         model = Sequential()
-        input_dim = 12
-        model.add(Dense(24, input_dim=input_dim, activation="relu"))
-        model.add(Dense(48, activation="relu"))
-        model.add(Dense(24, activation="relu"))
-        model.add(Dense(4))
-        model.compile(loss="mean_squared_error", optimizer=Adam(learning_rate=self.learning_rate))
+        model.add(Dense(128, input_dim=self.state_space, activation="relu"))
+        model.add(Dense(128, activation="relu"))
+        model.add(Dense(128, activation="relu"))
+        model.add(Dense(self.action_space, activation='softmax'))
+        model.compile(loss="mse", optimizer=Adam(learning_rate=self.learning_rate))
         return model
 
     def act(self, state):
-        self.epsilon *= self.epsilon_decay
-        self.epsilon = max(self.epsilon_min, self.epsilon)
-        if np.random.random() < self.epsilon:
-            return random.choice([0, 1, 2, 3])
-
-        prediction = self.model.predict(state, verbose=0)
-        return np.argmax(prediction[0])
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_space)
+        act_values = self.model.predict(state, verbose=0)
+        return np.argmax(act_values[0])
 
     def remember(self, state, action, reward, new_state, done):
-        self.memory.append([state, action, reward, new_state, done])
+        self.memory.append((state, action, reward, new_state, done))
 
     def replay(self):
         if len(self.memory) < self.batch_size:
             return
 
-        samples = random.sample(self.memory, self.batch_size)
-        for sample in samples:
-            state, action, reward, new_state, done = sample
-            target = self.target_model.predict(state, verbose=0)
-            if done:
-                target[0][action] = reward
-            else:
-                prediction = self.target_model.predict(new_state, verbose=0)
-                q_future = max(prediction[0])
-                target[0][action] = reward + q_future * self.gamma
-            self.model.fit(state, target, epochs=1, verbose=0)
+        minibatch = random.sample(self.memory, self.batch_size)
+        states = np.array([i[0] for i in minibatch])
+        actions = np.array([i[1] for i in minibatch])
+        rewards = np.array([i[2] for i in minibatch])
+        next_states = np.array([i[3] for i in minibatch])
+        dones = np.array([i[4] for i in minibatch])
 
-    def target_train(self):
-        weights = self.model.get_weights()
-        target_weights = self.target_model.get_weights()
-        for i in range(len(target_weights)):
-            target_weights[i] = weights[i] * self.tau + target_weights[i] * (1 - self.tau)
-        self.target_model.set_weights(target_weights)
+        states = np.squeeze(states)
+        next_states = np.squeeze(next_states)
+
+        targets = rewards + self.gamma * (np.amax(self.model.predict_on_batch(next_states), axis=1)) * (1 - dones)
+        targets_full = self.model.predict_on_batch(states)
+
+        ind = np.array([i for i in range(self.batch_size)])
+        targets_full[[ind], [actions]] = targets
+
+        self.model.fit(states, targets_full, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
 
     def save_model(self, fn):
         self.model.save(fn)
 
     def train(self):
-        for trial in range(self.trials):
+        for episode in range(self.episodes):
             self.env = DeepQEnvironment()
 
-            current_state = np.reshape(self.env.get_state(), (1, 12))
+            current_state = self.env.get_state()
+            current_state = np.reshape(current_state, (1, 12))
             score = 0
 
-            for step in range(self.trial_len):
+            for i in range(10000):
                 action = self.act(current_state)
                 new_state, reward, done = self.env.step(Direction(action))
                 new_state = np.reshape(new_state, (1, 12))
@@ -92,14 +92,11 @@ class DeepQTrainer:
 
                 self.remember(current_state, action, reward, new_state, done)
 
-                self.replay()  # internally iterates default (prediction) model
-                self.target_train()  # iterates target model
-
                 current_state = new_state
+
+                self.replay()
                 if done:
-                    print(f'episode: {trial + 1}/{self.trials}, score: {score + 10}')
+                    print(f'episode: {episode + 1}/{self.episodes}, score: {score}')
                     break
 
-            print("Failed to complete in trial {}".format(trial))
-            if trial % 10 == 0:
-                self.save_model("models/trial-{}.model".format(trial))
+        return
